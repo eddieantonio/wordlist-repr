d94dde1deda68bcee6c92b7359017691d7d6c415
Eddie Antonio Santos <easantos@ualberta.ca>
2019-02-14 14:17:14 -0700

    Let's create a simple array.

--- test/test-simple-array.js
+++ test/test-simple-array.js
@@ -1,9 +1,16 @@
 import test from 'ava';
+import fs from 'fs';
 
-import {name} from '../algorithms/simple-array';
+import simpleArray, {name} from '../algorithms/simple-array';
 
-/* TODO: Test that we can build it. */
-test.todo(`[${name}]: building`);
+
+test(`[${name}]: building`, t => {
+  let contents = fs.readFileSync('./build/SaanichWordFreq.raw-wordlist.js', 'UTF-8');
+  let dataStructure = simpleArray.build(contents);
+  t.truthy(dataStructure); 
+});
+
+test.todo(`[${name}]: serialize`);
 
 /* TODO: Test that we can lookup every element in in it */
 test.todo(`[${name}]: lookup`);
